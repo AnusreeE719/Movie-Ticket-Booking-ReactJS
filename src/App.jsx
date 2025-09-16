6d0672ec8579e89c8c0ec5b2084002a58246fc05
AnusreeE719 <anusreee719@gamil.com>
2025-09-16 21:28:26 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieTicketBooking from './components/MovieTicketBooking'
+
+const App = () => {
+  return (
+    <div>
+      <MovieTicketBooking />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MovieTicketBooking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieTicketBooking = () => {
+  return (
+    <div>
+      MovieTicketBooking
+    </div>
+  )
+}
+
+export default MovieTicketBooking
